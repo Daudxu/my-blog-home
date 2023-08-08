6c41b8f8da9c85b00d03387d7862ac69ff76c98d
xudan <875126243@qq.com>
2023-08-08 15:18:19 +0800

    fix update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
    <header class="py-3 flex justify-center items-center">
          <div class="flex justify-between items-center 2xl:container md:mx-auto">
-            <div class="cl-logo text-slate-950 font-bold ">
-                <font-awesome-icon class="text-lime-500 dark:text-blue-400"  icon="user-secret" />
+            <div class="cl-logo text-slate-950 font-bold rounded-xl shadow-xl p-2 flex justify-center items-center dark:text-slate-50 dark:bg-slate-700">
+                <font-awesome-icon class="text-lime-500 dark:text-blue-400 text-lg mr-2"  icon="user-secret" /><span >Logo</span>
             </div>
-            <nav class="flex justify-between items-center bg-slate-200 p-2 rounded-xl">
+            <nav class="flex justify-between items-center bg-slate-50 p-2 rounded-xl shadow-xl dark:text-slate-50 dark:bg-slate-700">
                 <div class="nav-links duration-500 md:static absolute  md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
                     <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
                         <li>
@@ -26,17 +26,19 @@
                 </div>
             </nav>
             <div class="flex items-center gap-3 md:gap-5">
-                <a  class="">Sign in</a>
-                <div class="w-9  md:w-12  bg-gray-500 rounded-full overflow-hidden">
-                <img class="w-full h-full object-cover"  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Avatar" />
+                <div class="flex justify-center items-center rounded-xl shadow-xl py-2 px-5 dark:text-slate-50 dark:bg-slate-700"> 
+                    <a  class="mr-2">Sign in</a>
+                    <div class="w-8  md:w-8  bg-gray-500 rounded-full overflow-hidden">
+                        <img class="w-full h-full object-cover"  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="Avatar" />
+                    </div>
                 </div>
-
-                <!-- sun icon -->
-                <svg v-if="isDark" @click="toggleIcon"  class="fill-current w-8 text-sky-900 dark:text-white cursor-pointer transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
-                
-                <!-- moon icon -->
-                <svg v-else @click="toggleIcon" class="fill-current w-8 text-sky-900 dark:text-white cursor-pointer transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
-                
+          
+                <div class="flex justify-center items-center rounded-xl shadow-xl p-2 dark:text-slate-50 dark:bg-slate-700">
+                    <!-- sun icon -->
+                    <svg v-if="isDark" @click="toggleIcon"  class="fill-current w-8 text-sky-900 dark:text-white cursor-pointer transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
+                    <!-- moon icon -->
+                    <svg v-else @click="toggleIcon" class="fill-current w-8 text-sky-900 dark:text-white cursor-pointer transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
+                </div>
                 <font-awesome-icon :icon="['fas', 'bars']" class="text-3xl cursor-pointer md:hidden text-sky-900 dark:text-white"></font-awesome-icon>
             </div>
          </div>
